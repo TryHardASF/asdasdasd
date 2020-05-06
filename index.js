b360c1f25f0973b2641bd927b3eabfbe12b22421
TryHardASF <62212428+TryHardASF@users.noreply.github.com>
2020-05-06 06:56:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ require("./server.js");
 const TOKEN = process.env.BOT_TOKEN;
 const PREFIX = process.env.PREFIX;
 const GOOGLE_API_KEY = process.env.YTAPI_KEY;
-@client.event
-async def on_ready():
-	activity = discord.Game(name= process.env.PREFIX"help")
-	await client.change_presence(status=discord.Status.online, activity=activity)
-const bot = new Client({
-    disableEveryone: true
+
 });
 
 const youtube = new YouTube(GOOGLE_API_KEY);
